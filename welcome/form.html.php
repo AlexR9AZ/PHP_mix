a0602e0dd60d05b915d8331cfbb493b0fad09161
AlexR9AZ <cb-404@mail.ru>
2021-06-20 08:44:04 +0500

    edited tabs

--- welcome/form.html.php
+++ welcome/form.html.php
@@ -7,12 +7,12 @@
 	<form action=" " method="post">
 		<div>
 			<label for="firstname">Имя:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				<input type="text" name="firstname" id="firstname">
+			<input type="text" name="firstname" id="firstname">
 			</label>
 		</div>
 		<div>
 			<label for="lastname">Фамилия:
-				<input type="text" name="lastname" id="lastname">
+			<input type="text" name="lastname" id="lastname">
 			</label>
 		</div>
 		<br>
